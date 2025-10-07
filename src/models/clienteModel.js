343153f0e49e3b01c6e6e8fd681229c31db1ccfc
yuinahirano <nandinhaa1280@gmail.com>
2025-10-07 16:21:05 -0300

    arrumando parte 2 da atividade

--- src/models/clienteModel.js
+++ src/models/clienteModel.js
@@ -1,48 +1,5 @@
 const { sql, getConnection } = require("../config/db");
 
-const produtoModel = {
-    // -------------------------------
-    // BUSCAR TODOS OS PRODUTOS
-    // -------------------------------
-    buscarTodos: async () => {
-        try {
-
-            const pool = await getConnection(); // Cria um conjunto de conexões com o BD
-            let sql = 'SELECT * FROM Produtos'; //seleciona os dados da tabela Produtos
-
-            const result = await pool.request().query(sql);
-
-            return result.recordset;
-
-        } catch (error) {
-            console.error('Error ao buscar produtos:', error);
-            throw error;// passa o erro para o controller tratar
-        }
-    },
-
-    // -------------------------------
-    // INSERIR NOVOS PRODUTOS
-    // -------------------------------
-
-    inserirProduto: async (nomeProduto, precoProduto) => {
-        try {
-            const pool = await getConnection();
-
-            let querySQL = 'INSERT INTO Produtos(nomeProduto, precoProduto) VALUES(@nomeProduto, @precoProduto)';//@ para chamar a variável
-
-            await pool.request()//requisição ao banco de dados
-                .input('nomeProduto', sql.VarChar(100), nomeProduto) //criar o nome da input, o tipo de dado que ela irá armazenar e o tipo da variável
-                .input('precoProduto', sql.Decimal(10, 2), precoProduto)
-                .query(querySQL) //aquisição
-
-        } catch (error) {
-            console.error('Erro ao inserir produto:', error);
-            throw error; //passa o erro para o controller tratar
-        }
-    }
-
-}
-
 const clienteModel = {
     // -------------------------------
     // BUSCAR TODOS OS CLIENTES
@@ -85,10 +42,4 @@ const clienteModel = {
 
 }
 
-module.exports = { produtoModel }
-
-//async function teste(){
-    //const produtos = await produtoModel.buscarTodos();
-    //console.log(produtos);
-//}
-//teste();
+module.exports = { clienteModel }
