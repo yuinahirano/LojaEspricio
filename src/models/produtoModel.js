bd79721eabf6a2eba91f04d0ab33363b9af2cf1d
yuinahirano <nandinhaa1280@gmail.com>
2025-10-07 16:21:35 -0300

    arrumado produtosModel

--- src/models/produtoModel.js
+++ src/models/produtoModel.js
@@ -43,47 +43,6 @@ const produtoModel = {
 
 }
 
-const clienteModel = {
-    // -------------------------------
-    // BUSCAR TODOS OS CLIENTES
-    // -------------------------------
-    buscarClientes: async () => {
-        try {
-    
-            const pool = await getConnection(); // Cria um conjunto de conexões com o BD
-            let sql = 'SELECT * FROM Clientes'; //seleciona os dados da tabela Clientes
-    
-            const result = await pool.request().query(sql);
-    
-            return result.recordset; //cria uma lista de clientes que vem como tabela e transforma em json
-    
-        } catch (error) {
-            console.error('Error ao buscar clientes:', error);
-            throw error;// passa o erro para o controller tratar
-        }
-    },
-    
-    // -------------------------------
-    // INSERIR NOVOS CLIENTES
-    // -------------------------------
-    inserirCliente: async (nomeCliente, cpfCliente) => {
-        try {
-            const pool = await getConnection();
-    
-            let querySQL = 'INSERT INTO Clientes(nomeCliente, cpfCliente) VALUES(@nomeCliente, @cpfCliente)';//@ para chamar a variável
-    
-            await pool.request()//requisição ao banco de dados
-                .input('nomeCliente', sql.VarChar(100), nomeCliente) //nome do input nomeClietes, irá armazenar um dado tipo varchar e a variável é nomeCliente
-                .input('cpfCliente', sql.Char(14), cpfCliente)//nome do input cpfClietes, irá armazenar um dado tipo char e a variável é cpfCliente
-                .query(querySQL) //aquisição
-    
-        } catch (error) {
-            console.error('Erro ao inserir produto:', error);
-            throw error; //passa o erro para o controller tratar
-        }
-    }
-
-}
 
 module.exports = { produtoModel }
 
